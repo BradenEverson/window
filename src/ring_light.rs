92e3b6452b01a9956d172c75faea6a3513643ed3
BradenEverson <bradeneverson@gmail.com>
2025-09-27 11:56:52 -0500

    Move outputpin

--- src/ring_light.rs
+++ src/ring_light.rs
@@ -3,10 +3,12 @@
 use std::thread;
 use std::time::Duration;
 
+use rppal::gpio::{Gpio, OutputPin};
+
 /// Control for a neopixel ring using GPIO bit banging
 pub struct NeoPixelRing {
     /// GPIO pin number for data line
-    data_pin: u8,
+    data_pin: OutputPin,
 }
 
 impl NeoPixelRing {
@@ -14,8 +16,11 @@ impl NeoPixelRing {
         if let Err(e) = rppal::gpio::Gpio::new() {
             return Err(format!("Failed to initialize GPIO: {}", e));
         }
+        let gpio = Gpio::new().unwrap();
 
-        Ok(Self { data_pin })
+        Ok(Self {
+            data_pin: gpio.get(data_pin).unwrap().into_output(),
+        })
     }
 
     pub fn light_em_up(&mut self, leds: u8) -> Result<(), String> {
@@ -35,14 +40,6 @@ impl NeoPixelRing {
     }
 
     fn write_rgb(&mut self, rgb_values: &[(u8, u8, u8)]) -> Result<(), String> {
-        let gpio =
-            rppal::gpio::Gpio::new().map_err(|e| format!("GPIO initialization failed: {}", e))?;
-
-        let mut pin = gpio
-            .get(self.data_pin)
-            .map_err(|e| format!("Failed to get GPIO pin {}: {}", self.data_pin, e))?
-            .into_output();
-
         const T0H: u64 = 350;
         const T0L: u64 = 800;
         const T1H: u64 = 700;
@@ -57,21 +54,21 @@ impl NeoPixelRing {
                     let bit_value = (byte >> bit) & 1;
 
                     if bit_value == 1 {
-                        pin.set_high();
+                        self.data_pin.set_high();
                         thread::sleep(Duration::from_nanos(T1H));
-                        pin.set_low();
+                        self.data_pin.set_low();
                         thread::sleep(Duration::from_nanos(T1L));
                     } else {
-                        pin.set_high();
+                        self.data_pin.set_high();
                         thread::sleep(Duration::from_nanos(T0H));
-                        pin.set_low();
+                        self.data_pin.set_low();
                         thread::sleep(Duration::from_nanos(T0L));
                     }
                 }
             }
         }
 
-        pin.set_low();
+        self.data_pin.set_low();
         thread::sleep(Duration::from_nanos(RESET));
 
         Ok(())
